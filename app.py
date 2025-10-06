abe16d4b1eb636fde0e56e11ef6b2679986dfa13
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2025-10-06 21:49:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     name = "Zainab Adebayo Temidun"
     playlist_url = "https://open.spotify.com/playlist/0sNfSkRkYaVQCwRYkAZZjG?si=e59954fc17dc4136"
-    with open('Data.json', 'r') as f:
+    with open('data.json', 'r') as f:
         songs = json.load(f)
 
     return render_template('index.html', name=name, playlist_url=playlist_url, songs=songs)
